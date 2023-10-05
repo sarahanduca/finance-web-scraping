a8e5aab15753479cf051aabc10c2cc89dcf450b5
sarahanduca <anduca.sarah@gmail.com>
2023-10-04 21:14:22 -0300

    adiciona pesquisa com conteudo gerado por JS

---
+++ get_dynamic_data.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import time
+
+def get_dynamic_data(stock_code):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome_options)
+    url = f"https://finance.yahoo.com/quote/{stock_code}?p={stock_code}"
+    driver.get(url)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    news_list = soup.find_all("li", class_="js-stream-content")
+    news_results = []
+    for news in news_list:
+        div = news.find("div")
+        if(div.has_attr("data-test-locator") and div["data-test-locator"] == "mega"):
+            news_results.append(
+                {
+                    "link": news.select_one("div > div > div > h3 > a")["href"] if news.select_one("div > div > div > h3 > a") else None,
+                    "title": news.find("h3").get_text(),
+                }
+            )
+    driver.quit()
+    return news_results
---
+++ yahoo_finance_news.py
@@ -0,0 +1,4 @@
+from get_dynamic_data import get_dynamic_data
+
+news_data = get_dynamic_data("ITUB4.SA")
+print(news_data)
